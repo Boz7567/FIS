acfab8b2741f00082cf597cb1f46a2499ca0dfab
Boz7567 <aleboz.asquasciati@gmail.com>
2025-11-05 14:37:36 +0100

    lab2 serio

--- lab2/assignment/app/src/main/java/assignment/user/User.java
+++ lab2/assignment/app/src/main/java/assignment/user/User.java
@@ -7,12 +7,18 @@ import assignment.cart.Cart;
 
 import java.util.ArrayList; 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sun.org.slf4j.internal.LoggerFactory;
+
 public class User {
     private String userID; 
     private String username; 
     private String firstname; 
     private String lastname;
-    private List<String> titles = new ArrayList<String>();
+    // private List<String> titles = new ArrayList<String>(); //problema di Maintainability
+    private List<String> titles = new ArrayList<>(); //versione corretta
     private String[] roles = new String[5];
     private boolean accountActive;
     private Cart cart;
@@ -65,13 +71,12 @@ public class User {
     }
 
     public boolean isActive(){
-        if(accountActive)
-            return true;
-        return false;
+        //problema di Maintainability
+        return isAccountActive();
     }
 
     public boolean deactivateAccount(String id) {
-        if (accountActive && this.userID == id) {
+        if (accountActive && this.userID.equals(id)) { //problema di Reliability
             accountActive = false;
             return true;
         }
@@ -79,16 +84,17 @@ public class User {
     }
 
     public boolean isEquals(User u){
-        return u.userID == this.userID;
+        return u.userID.equals(this.userID); //problema di Reliability
     }
 
     public void printUserInfo() {
-        System.out.println("User Info: " + firstname + " " + lastname + " (Username: " + username + ")");
+        // System.out.println("User Info: " + firstname + " " + lastname + " (Username: " + username + ")"); //problema di Maintainability
+        logger.info("User Info: " + firstname + " " + lastname + " (Username: " + username + ")");
     }
 
-    public void linkCart(Cart cart) throws Exception{
+    public void linkCart(Cart cart) throws IllegalArgumentException{
         if(cart == null)
-            throw new Exception();
+            throw new IllegalArgumentException(); //problema di Mantainability
         this.cart = cart;
     }
 
@@ -97,12 +103,14 @@ public class User {
     }
 
     public String printAllRoles(){
-        return roles.toString();
+        return Arrays.toString(roles);  //problema di Reliability
     }
+    private static final Logger logger = LoggerFactory.getLogger(User.java);
 
-    public void PrintEveryRole(){
-        for (int i = roles.length; i > 0; i++){
-            System.out.println(roles[i]);
+    public void printEveryRole(){   //problema di Mantainability
+        for (int i = roles.length; i > 0; i--){ //problema di Reliability
+            // System.out.println(roles[i]);   //problema di Maintainability
+            logger.log(roles[i]);
         }
     }
 
